cae638a7f26b264f0566720b081037fdb793dd0b
Ryan Simpson <ryan999000999@hotmail.co.uk>
2019-06-24 07:46:21 +0100

    added longer test files

---
+++ data/generateTestData.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+let udprns = 'udprns' + '\r\n';
+let mask = '00000000'
+// for(let i = 7500000; i < 12500000; i++) {
+//   let strNum = String(i)
+//   let numLength = strNum.length
+//   udprns += mask.slice(numLength) + strNum + '\r\n'
+// }
+for(let i = 0; i < 10000000; i++) {
+  let strNum = String(i)
+  let numLength = strNum.length
+  udprns += mask.slice(numLength) + strNum + '\r\n'
+}
+
+
+// write to a new file named 2pac.txt
+fs.writeFile('A_long.csv', udprns, (err) => {  
+    // throws an error, you could also catch it here
+    if (err) throw err;
+
+    // success case, the file was saved
+    console.log('Lyric saved!');
+});
